af54a6c0f069588e0613e1a53549d76c09378038
shubham88fru <shubham88fru@yahoo.com>
2025-03-28 15:50:14 -0700

    nav header complete

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,27 @@
+import { cn } from "@/lib/utils";
+import { useAuth } from "@clerk/clerk-react";
+import Container from "./ui/container";
+import LogoContainer from "./LogoContainer";
+import NavigationRoutes from "./navigation-routes";
+
 const Header = () => {
-  return <div>Header</div>;
+  const { userId } = useAuth();
+
+  return (
+    <header
+      className={cn("w-full border-b duration-150 transition-all ease-in-out")}
+    >
+      <Container>
+        <div className="flex items-center gap-4 w-full">
+          <LogoContainer />
+
+          <nav className="hidden md:flex items-center gap-3">
+            <NavigationRoutes />
+          </nav>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
